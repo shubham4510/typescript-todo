ff0a676ed131baa53d3ef7bef799063664e3bcbd
Shubham <shubhambhardwaj112277@gmail.com>
2024-04-09 21:58:34 +0530

    Added delete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,41 @@ import { useState } from "react"
 // import { Button } from "@mui/base"
 
 const App = () => {
-  const [todos,setTodos] = useState<TodoItemType[]>([
-    {
-      title:"hey",
-      isCompleted:false,
-      id:"s"
-    },
-    {
-      title:"hello",
-      isCompleted:false,
-      id:"os"
-    },
-  ]);
+  const [title,setTitle] = useState<TodoItemType[]>([]);
+  const [todos,setTodos] = useState<TodoItemType["title"]>([]);
 
   const completeHandler = (id:TodoItemType["id"]):void =>{
-    alert(id)
+    const newTodos:TodoItemType = todos.map(i=>{
+      if(i.id===id)i.isCompleted = !i.isCompleted
+      return i;
+    })
+   setTodos(newTodos)
+    
   }
   const deleteHandler = (id:TodoItemType["id"]):void =>{
-    alert(id)
+    
+    const newTodos:TodoItemType = todos.filter(i=>i.id!==id);
+   setTodos(newTodos)
   }
+
+  const editHandler = (id:TodoItemType["id"],newTitle:TodoItemType["title"]):void =>{
+    const newTodos:TodoItemType = todos.map(i=>{
+      if(i.id===id)i.title = !newTitle
+      return i;
+    })
+   setTodos(newTodos)
+    
+  }
+
+  const submitHandler = ():void=>{
+    const newTodo:TodoItemType = {
+      title,
+      isCompleted:false,
+      id:String(Math.random()*1000)
+    }
+    setTodos(prev=>([...prev,newTodo]))
+    setTitle("")
+  };
 
   return <Container maxWidth="sm" sx={{height:"100vh"}}>
     <AppBar position="static">
@@ -35,11 +51,13 @@ const App = () => {
     </AppBar>
     <Stack height={"73%"} direction={"column"} spacing={"1rem"} p={"1rem"}>
     {
-      todos.map((i)=> <TodoItem deleteHandler={deleteHandler} completeHandler={completeHandler} key={i.id} todo={i}/>)
+      todos.map((i)=> <TodoItem editHandler={editHandler} deleteHandler={deleteHandler} completeHandler={completeHandler} key={i.id} todo={i}/>)
     }
     </Stack>
-    <TextField fullWidth label={"New Task"} />
-    <Button sx={{margin:"1rem 0"}} fullWidth variant={"contained"}>ADD</Button>
+    <TextField value={title} onChange={(e)=>setTitle(e.target.value)} fullWidth label={"New Task"} onKeyDown={(e)=>{
+      if(e.key==="Enter" && title!=="") submitHandler();
+    }} />
+    <Button sx={{margin:"1rem 0"}} fullWidth variant={"contained"} onClick={submitHandler} disabled={title ===""}>ADD</Button>
    
   </Container>
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,19 +1,33 @@
-import { Checkbox, Paper, Typography, Button } from "@mui/material";
+import { Checkbox, Paper, Typography, Button, TextField } from "@mui/material";
 import Stack from "@mui/system/Stack"; // Import Stack correctly
+import { useState } from "react";
 
 type PropsType = {
   todo: TodoItemType;
   deleteHandler: (id: TodoItemType["id"]) => void;
   completeHandler: (id: TodoItemType["id"]) => void;
+  editHandler: (id: TodoItemType["id"],string:TodoItemType["title"]) => void;
 };
 
-const TodoItem = ({ todo, deleteHandler, completeHandler }: PropsType) => {
+const TodoItem = ({ todo, deleteHandler, completeHandler, editHandler }: PropsType) => {
+  const [editActive, setEditActive] = useState<boolean>(false);
+  const [textVal, setTextVal] = useState<string>(todo.title);
+
   return (
     <Paper sx={{ padding: "1rem" }}>
       <Stack direction="row" alignItems="center">
-        <Typography marginRight="auto">{todo.title}</Typography>
-        <Checkbox />
-        <Button onClick={() => completeHandler(todo.id)}>Edit</Button>
+        {editActive ? (
+          <TextField value={textVal} onChange={(e) => setTextVal(e.target.value)} onKeyDown={(e)=>{
+            if(e.key==="Enter" && textVal!=="") {
+              editHandler(todo.id,textVal);
+              setEditActive(false)
+            }
+          }} />
+        ) : (
+          <Typography marginRight="auto">{todo.title}</Typography>
+        )}
+        <Checkbox checked={todo.isCompleted} onChange={() => completeHandler(todo.id)} />
+        <Button onClick={() => setEditActive((prev) => !prev)}>Edit</Button>
         <Button onClick={() => deleteHandler(todo.id)}>🗑️</Button>
       </Stack>
     </Paper>
